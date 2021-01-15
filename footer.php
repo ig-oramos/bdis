10bdae81540044ea8230e76ee4c8a0f0148d6ce3
ig-oramos <56934942+ig-oramos@users.noreply.github.com>
2021-01-15 16:21:33 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,13 @@
+</div>
+<footer class="rodape">
+	<div class="social-icons">
+		<a href="https://goo.gl/yaFkJe"><i class="fa fa-facebook"></i></a>
+		<a href="https://github.com/ig-oramos/bdis"><i class="fa fa-github"></i></a>
+		<a href="https://linkedin.com/in/igor-g-o-ramos-4637b1178"><i class="fa fa-linkedin"></i></a>
+	</div>
+	<p class="copyright">Copyright &copy; 2018 - <?php if (date('Y') > 2018)echo date('Y');?>
+	 by Igor G. O. Ramos</p>
+</footer>
+</div>
+</body>
+</html>
